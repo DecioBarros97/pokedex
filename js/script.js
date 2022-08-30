460c223b75fa2932d669559ccf18c0e79b6ce782
Decio Matheus <deciobarros97@gmail.com>
2022-08-29 23:37:52 -0300

    att

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@ const pokemonName = document.querySelector('.pokemon__name');
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-     const data = await APIResponse.json();
-    return data;
+    
+    const data = await APIResponse.json();
+   return data;
 
-
+   
 } 
+
 //função q vai renderizar esses dados na tela 
 
 const rederPokemon = async (pokemon) => {
-
-    const data = await fetchPokemon(pokemon);
+    
+const data = await fetchPokemon(pokemon);
 
 }
